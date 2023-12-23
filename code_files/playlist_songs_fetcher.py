a782def630b8d64cbe67c9d1260b0d90d214d7fa
Navadeep Naidu <84763359+navadeepnaidu7@users.noreply.github.com>
2023-12-23 19:07:08 +0530

    updated comments for better explanation

--- code_files/playlist_songs_fetcher.py
+++ code_files/playlist_songs_fetcher.py
@@ -1,5 +1,4 @@
 import os
-from redis import UsernamePasswordCredentialProvider
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import google_auth_oauthlib.flow
@@ -7,19 +6,19 @@ import googleapiclient.discovery
 import googleapiclient.errors
 
 
-# Retrieve API credentials from environment variables
+# Spotify API credentials (defined as environment variables)
 client_id = os.environ.get("SPOTIPY_CLIENT_ID")
 client_secret = os.environ.get("SPOTIPY_CLIENT_SECRET")
 redirect_uri = os.environ.get("SPOTIPY_REDIRECT_URI")
 
-# Authenticate with Spotify
+# Authentication with Spotify
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-# Set up YouTube API environment variables
+# YouTube API environment variables
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\navad\Downloads\client_secrets.json"   # Replace with your credentials path
 
-# Authenticate with YouTube
+# Authentication with YouTube
 scopes = ["https://www.googleapis.com/auth/youtube"]
 client_secrets_file = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
 flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
@@ -28,18 +27,18 @@ flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
 credentials = flow.run_local_server(port=0)
 youtube = googleapiclient.discovery.build("youtube", "v3", credentials=credentials)
 
-# Get user's authorization
+# user's authorization
 scope = "playlist-read-private"
 token = spotipy.util.prompt_for_user_token(client_id=client_id,
-                                          client_secret=client_secret,
-                                          redirect_uri=redirect_uri,
-                                          scope=scope)
+                                                        client_secret=client_secret,
+                                                        redirect_uri=redirect_uri,
+                                                        scope=scope)
 if token:
-   sp = spotipy.Spotify(auth=token)
+    sp = spotipy.Spotify(auth=token)
 else:
-   print("Can't get token for", UsernamePasswordCredentialProvider)
+    print("Can't get token")
 
-# Get user's playlists
+# Getting user's playlists
 playlists = sp.current_user_playlists()
 
 # Display available playlists
@@ -47,11 +46,11 @@ print("Your Playlists:")
 for i, playlist in enumerate(playlists['items']):
    print(f"{i + 1}. {playlist['name']}")
 
-# Ask user to select a playlist
+# User selects a playlist
 selected_playlist_index = int(input("Select a playlist by number: ")) - 1
 selected_playlist_id = playlists['items'][selected_playlist_index]['id']
 
-# Retrieve tracks from the selected playlist
+# Extract tracks from the selected playlist
 playlist_tracks = sp.playlist_tracks(selected_playlist_id)
 
 # Display song names with artist names
@@ -61,13 +60,13 @@ for track in playlist_tracks['items']:
    artist_name = track['track']['artists'][0]['name']
    print(f"- {song_name} - {artist_name}")
 
-   # Create a new YouTube playlist
+   # Creates a new YouTube playlist
 request = youtube.playlists().insert(
     part="snippet,status",
     body={
         "snippet": {
             "title": playlists['items'][selected_playlist_index]['name'],
-            "description": "Playlist transferred from Spotify Created By Navadeep Naidu"
+            "description": "Playlist transferred from Spotify"
         },
         "status": {
             "privacyStatus": "PUBLIC"  # Adjust privacy if needed
@@ -77,7 +76,7 @@ request = youtube.playlists().insert(
 playlist_response = request.execute()
 youtube_playlist_id = playlist_response["id"]
 
-# Search for songs on YouTube and add to playlist
+# Searches for songs on YouTube and add it to playlist
 for track in playlist_tracks['items']:
     song_name = track['track']['name']
     artist_name = track['track']['artists'][0]['name']
