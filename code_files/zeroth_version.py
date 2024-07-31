3d2865c272de9bea4496e11dfc2b85dd999eb139
Navadeep Naidu <84763359+navadeepnaidu7@users.noreply.github.com>
2024-08-01 00:01:06 +0530

    minor changes in code for optimising api calls

--- code_files/zeroth_version.py
+++ code_files/zeroth_version.py
@@ -5,7 +5,6 @@ import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
-
 # Spotify API credentials (defined as environment variables)
 client_id = os.environ.get("SPOTIPY_CLIENT_ID")
 client_secret = os.environ.get("SPOTIPY_CLIENT_SECRET")
@@ -16,7 +15,7 @@ client_credentials_manager = SpotifyClientCredentials(client_id=client_id, clien
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 # YouTube API environment variables
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\navad\Coding\spotify_sync_app\text_files\client_secrets.json"   # Replace with your credentials path
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\navad\Coding\spotify_sync_app\code_files\client_secrets.json"   # Replace with your credentials path
 
 # Authentication with YouTube
 scopes = ["https://www.googleapis.com/auth/youtube"]
@@ -27,16 +26,17 @@ flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
 credentials = flow.run_local_server(port=0)
 youtube = googleapiclient.discovery.build("youtube", "v3", credentials=credentials)
 
-# user's authorization
+# User's authorization
 scope = "playlist-read-private"
 token = spotipy.util.prompt_for_user_token(client_id=client_id,
-                                                        client_secret=client_secret,
-                                                        redirect_uri=redirect_uri,
-                                                        scope=scope)
+                                           client_secret=client_secret,
+                                           redirect_uri=redirect_uri,
+                                           scope=scope)
 if token:
     sp = spotipy.Spotify(auth=token)
 else:
     print("Can't get token")
+    exit()
 
 # Getting user's playlists
 playlists = sp.current_user_playlists()
@@ -53,14 +53,7 @@ selected_playlist_id = playlists['items'][selected_playlist_index]['id']
 # Extract tracks from the selected playlist
 playlist_tracks = sp.playlist_tracks(selected_playlist_id)
 
-# Display song names with artist names
-print("Songs in the playlist:")
-for track in playlist_tracks['items']:
-   song_name = track['track']['name']
-   artist_name = track['track']['artists'][0]['name']
-   print(f"- {song_name} - {artist_name}")
-
-   # Creates a new YouTube playlist
+# Creates a new YouTube playlist
 request = youtube.playlists().insert(
     part="snippet,status",
     body={
@@ -76,19 +69,27 @@ request = youtube.playlists().insert(
 playlist_response = request.execute()
 youtube_playlist_id = playlist_response["id"]
 
-# Searches for songs on YouTube and add it to playlist
+# Display song names with artist names and prepare search requests
+song_requests = []
 for track in playlist_tracks['items']:
     song_name = track['track']['name']
     artist_name = track['track']['artists'][0]['name']
+    print(f"- {song_name} - {artist_name}")
 
     search_request = youtube.search().list(
         part="snippet",
         q=f"{song_name} {artist_name}",
         maxResults=1,  # Retrieve only the top result
-        type="VIDEO"
+        type="video"
     )
-    search_response = search_request.execute()
+    song_requests.append(search_request)
 
+# Perform the search and add songs to YouTube playlist
+for search_request, track in zip(song_requests, playlist_tracks['items']):
+    song_name = track['track']['name']
+    artist_name = track['track']['artists'][0]['name']
+
+    search_response = search_request.execute()
     if search_response["items"]:
         video_id = search_response["items"][0]["id"]["videoId"]
         add_video_request = youtube.playlistItems().insert(
